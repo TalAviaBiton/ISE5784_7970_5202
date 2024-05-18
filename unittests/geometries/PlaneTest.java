a33b5a58be2167e348020983101689f119c30273
tal217@ultalya.com <tal217@ultalya.com>
2024-05-19 02:10:24 +0300

    test shapes

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -1,10 +1,15 @@
 package geometries;
-
+import primitives.Point;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlaneTest {
+
+    public PlaneTest(Point p1, Point p2, Point p3){
+
+
+    }
 
     @Test
     void testGetNormal() {
