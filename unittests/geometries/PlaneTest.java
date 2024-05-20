71370aafe6ceb6a358f31ac76d262b31492c3c06
Roze Aranbayev <aranbaye@acad.jct.ac.il>
2024-05-20 17:53:31 +0300

    plane

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -3,19 +3,32 @@ import primitives.Point;
 import org.junit.jupiter.api.Test;
 import primitives.Vector;
 
+import java.security.InvalidAlgorithmParameterException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlaneTest {
 
-    public PlaneTest(Point p1, Point p2, Point p3){
-    assertEquals(
-            p1,
-            p2,
+    public PlaneTest(){
+        Point p1=new Point(1,1,1);
+        Point p2=new Point(1,1,1);
+        Point p3=new Point(2,1,1);
+
+        assertThrows(
+            IllegalArgumentException.class,
+            () -> {Plane plane = new Plane(p1, p2, p3);},
             "ERROR: testGetNormal() two points of the plane are equal");
-   assertEquals(
-                true,
-                p1.isOnSameLine(p2) || p1.isOnSameLine(p3) || p2.isOnSameLine(p3),
-                 "ERROR: testGetNormal() two points of the plane are on the same line");
+
+        Point p4=new Point(1,0,0);
+        Point p5=new Point(2,0,0);
+        assertThrows(
+            InvalidAlgorithmParameterException.class,
+            () -> {Plane plane = new Plane(p1, p4, p5);},
+            "ERROR: testGetNormal() two points of the plane are on the same line");
+//   assertEquals(
+//                true,
+//                p1.isOnSameLine(p2) || p1.isOnSameLine(p3) || p2.isOnSameLine(p3),
+//                 "ERROR: testGetNormal() two points of the plane are on the same line");
 
 
     }
--- unittests/geometries/TriangleTest.java
+++ unittests/geometries/TriangleTest.java
@@ -2,12 +2,17 @@ package geometries;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TriangleTest {
     @Test
     void testGetNormal()
     {
+        Triangle triangle = new Polygon((1,1,1),(2,2,2),(3,3,3));
+
+
 
     }
 
