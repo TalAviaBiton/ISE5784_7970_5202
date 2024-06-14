f6405b5e10f05a8283d1a3103956855a75d001ce
Roze Aranbayev <aranbaye@acad.jct.ac.il>
2024-06-14 18:35:47 +0300

    did some of th intersections

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -5,7 +5,9 @@ package geometries;
 
 import primitives.Point;
 import primitives.Ray;
+import primitives.Vector;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Triangle extends Polygon {
@@ -21,6 +23,25 @@ public class Triangle extends Polygon {
     }
      @Override
    public List<Point> findIntersections(Ray ray) {
-      return null;
+
+        Plane plane =new Plane(this.vertices.get(0),this.vertices.get(1),this.vertices.get(2));
+         var intersectionPlane=plane.findIntersections(ray);
+
+         Vector v1= this.vertices.get(0).subtract(ray.getHead());
+         Vector v2= this.vertices.get(1).subtract(ray.getHead());
+         Vector v3= this.vertices.get(2).subtract(ray.getHead());
+
+         Vector n1=v1.crossProduct(v2).normalize();
+         Vector n2=v2.crossProduct(v3).normalize();
+         Vector n3=v3.crossProduct(v1).normalize();
+
+         if((ray.getDirection().dotProduct(n1)>0
+                 && ray.getDirection().dotProduct(n2)>0
+                 && ray.getDirection().dotProduct(n3)>0) ||
+                        (ray.getDirection().dotProduct(n1)<0
+                         && ray.getDirection().dotProduct(n2)<0
+                         && ray.getDirection().dotProduct(n3)<0))
+            return intersectionPlane;
+        return null;
    }
 }
