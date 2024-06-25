e83075cfeffc2681bbc6dfa02c1b580f2d179d51
tal217@ultalya.com <tal217@ultalya.com>
2024-06-25 11:57:10 +0300

    stage 3 finished

--- unittests/geometries/GeomtriesTest.java
+++ unittests/geometries/GeomtriesTest.java
@@ -18,38 +18,38 @@ class GeomtriesTest {
     @Test
     void testFindIntersections() {
 
-        Sphere sphere1 = new Sphere(new Point(1.04, 7.24, 0), 1);
-        Sphere sphere134 = new Sphere(new Point(1.57, 1.68, 0), 2);
+        Sphere sphere1 = new Sphere(new Point(1, 7, 0), 1);
+        Sphere sphere134 = new Sphere(new Point(1, 1, 0), 2);
         Sphere sphere2 = new Sphere(new Point(0, 4, 0), 1);
-        Sphere sphere3 = new Sphere(new Point(-3.4, 3.13, 0), 1.5);
+        Sphere sphere3 = new Sphere(new Point(-3, 3, 0), 1.5);
 
         Point[] points1 =
-                {new Point(6.66, 2.84, 0),
-                        new Point(3.98, 5.15, 0),
-                        new Point(5.89, 6.67, 0)
+                {new Point(7, 3, 0),
+                        new Point(4, 5, 0),
+                        new Point(6, 7, 0)
                 };
         Triangle triangle1 = new Triangle(points1[0], points1[1], points1[2]);
         Point[] points2 =
-                {new Point(-4.3, 1.33, 0),
-                        new Point(-3.89, 4.13, 0),
-                        new Point(-2.32, 1.66, 0)
+                {new Point(-4, 1, 0),
+                        new Point(-4, 4, 0),
+                        new Point(-2, 2, 0)
                 };
         Triangle triangle2 = new Triangle(points2[0], points2[1], points2[2]);
 
         Point[] points3 =
-                {new Point(-1.46, -3.04, 0),
-                        new Point(-2.32, 0, 0),
+                {new Point(-1, -3, 0),
+                        new Point(-2, 0, 0),
                         new Point(0, -3, 0)
                 };
         Triangle triangle134 = new Triangle(points3[0], points3[1], points3[2]);
 
 
         Plane plane14 = new Plane(
-                new Point(-4, 0, 0),
-                new Point(0, 0, 5.72), new Point(2.27, -1.42, 0));
+                new Point(-25, 0, 0),
+                new Point(0, 0, 10), new Point(-10, 0, 0));
         Plane plane2 = new Plane(
                 new Point(1, -4, 0),
-                new Point(3.64, -4.75, 0), new Point(2.34, -2.26, 0));
+                new Point(4, -5, 0), new Point(2, -2, 0));
 
 
         // ============ Equivalence Partitions Tests ==============
@@ -57,22 +57,19 @@ class GeomtriesTest {
         Geometries geometries1 = new Geometries();
         geometries1.geometries.add(sphere1);
         geometries1.geometries.add(triangle1);
-        geometries1.geometries.add(sphere134);
         geometries1.geometries.add(plane14);
         geometries1.geometries.add(triangle134);
 
         List<Point> expected1 = new LinkedList<>(List.of(
-                new Point(2.58, -1.38, 0),
-                new Point(2.36, -0.15, 0),
-                new Point(1.68, 3.68, 0),
-                new Point(1.22, 6.25, 0),
-                new Point(0.87, 8.22, 0)));
+                new Point(1, 8, 0),
+                new Point(1,6, 0),
+                new Point(1, 0, 0)));
 
-        Ray ray1 = new Ray(new Point(-2.24, -5.13, 0), new Vector(3.44, 11.49, 0));
+        Ray ray1 = new Ray(new Point(1, -5, 0), new Vector(0, 15, 0));
         assertEquals(
                 expected1,
                 geometries1.findIntersections(ray1),
-                "Error: doesnt return the right intersection points " +
+                "Error: doesn't return the right intersection points " +
                         "when some (but not all) of the shapes has intersection");
 
         // =============== Boundary Values Tests ==================
