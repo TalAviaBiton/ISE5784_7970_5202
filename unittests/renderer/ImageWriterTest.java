51458107af0be41453bdec4f7c33522904fada9a
tal217@ultalya.com <tal217@ultalya.com>
2024-07-14 10:45:26 +0300

    stage 6 th calcColor

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -5,7 +5,7 @@ import primitives.Color;
 
 public class ImageWriterTest {
     @Test
-    void writeToImage() {
+    void testWriteToImage() {
         ImageWriter imageWriter=new ImageWriter("yellow", 800, 500);
         for(int i=0; i<imageWriter.getNx();i++) {
             for(int j=0; j<imageWriter.getNy();j++) {
