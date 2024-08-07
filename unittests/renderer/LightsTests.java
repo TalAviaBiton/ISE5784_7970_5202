eef9bfbc51a7af861194b139830ba03f8c85a087
Roze Aranbayev <aranbaye@acad.jct.ac.il>
2024-08-07 18:11:01 +0300

    stage 6 almost finished

--- unittests/renderer/LightsTests.java
+++ unittests/renderer/LightsTests.java
@@ -16,19 +16,19 @@ import java.util.List;
  * @author Dan
  */
 public class LightsTests {
-    /** First scene for some of tests */
+    /** First scene for some of the tests */
     private final Scene          scene1                  = new Scene("Test scene");
-    /** Second scene for some of tests */
+    /** Second scene for some of the tests */
     private final Scene          scene2                  = new Scene("Test scene")
             .setAmbientLight(new AmbientLight(new Color(WHITE), new Double3(0.15)));
 
-    /** First camera builder for some of tests */
+    /** First camera builder for some of the tests */
     private final Camera.Builder camera1                 = Camera.getBuilder()
             .setRayTracer(new SimpleRayTracer(scene1))
             .setLocation(new Point(0, 0, 1000))
             .setDirection(new Vector(0,0,-1), new Vector(0,1,0))
             .setVpSize(150, 150).setVpDistance(1000);
-    /** Second camera builder for some of tests */
+    /** Second camera builder for some of the tests */
     private final Camera.Builder camera2                 = Camera.getBuilder()
             .setRayTracer(new SimpleRayTracer(scene2))
             .setLocation(new Point(0, 0, 1000))
